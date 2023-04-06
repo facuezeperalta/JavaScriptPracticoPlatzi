820f39adfa94d79edd9597a9a4e370b22a2ddbd9
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2023-04-05 21:50:40 -0300

    se han hechos muchas cosas

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log('Prueba');
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const parrafito = document.querySelector('.parrafito');
+const pid = document.querySelector('#pid');
+const input = document.querySelector('input');
+
+
+
+console.log({
+    h1,
+    p,
+    parrafito,
+    pid,
+    input,
+});
+
+
+h1.innerHTML = 'A la grande le puse cuca <br> by homero'; // se pueden agergar etiquetas html también.
+
+/* console.log(h1.getAttribute('pantalla'));
+
+h1.setAttribute('class', 'rojo'); */
+
+/* h1.classList.add('green');
+h1.classList.remove('verde'); */
+
+/* input.value= '45678910'; */ //cambio el valor de Value del input.
+
+
+//crear elemetno en el dom
+
+
+const imagen = document.createElement('img');
+
+imagen.setAttribute('src','https://1.img-dpreview.com/files/p/TC3840x2560S3840x2560~sample_galleries/3220969392/5842928524.jpg');
+
+pid.append(imagen);
+
+
